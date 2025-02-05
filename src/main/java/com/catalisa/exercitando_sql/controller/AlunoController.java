58a0dbfed66af60f82d01b95a4add91be27e95e9
Luana Silva <luana.dossantos@zup.com.br>
2025-02-05 15:59:57 -0300

    feat: adiciona API Get em AlunoController

--- src/main/java/com/catalisa/exercitando_sql/controller/AlunoController.java
+++ src/main/java/com/catalisa/exercitando_sql/controller/AlunoController.java
@@ -1,4 +1,26 @@
 package com.catalisa.exercitando_sql.controller;
 
+import com.catalisa.exercitando_sql.dto.AlunoDTO;
+import com.catalisa.exercitando_sql.service.AlunoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
 public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AlunoDTO>> listarTodosProdutos () {
+        return ResponseEntity.ok(alunoService.listarTodosAlunos());
+    }
 }
